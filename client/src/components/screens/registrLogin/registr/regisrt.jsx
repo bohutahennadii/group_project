e5e0f1051faf9ea9cb9e2d7c924671dce73864b0
VoronovskiyVolodymyr <volodimir.voronovskiy@gmail.com>
2024-05-06 23:46:02 +0300

    new backend

--- client/src/components/screens/registrLogin/registr/regisrt.jsx
+++ client/src/components/screens/registrLogin/registr/regisrt.jsx
@@ -1,21 +1,52 @@
 import React from 'react'
 import style from './registr.module.css'
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function Registr(){
+  const [formData, setFormData] = useState({
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Password: '',
+    
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5042/api/Registration/registration', formData);
+      console.log('Успішно відправлено:', response.data);
+    } catch (error) {
+      console.error('Помилка відправки:', error);
+    }
+  };
+  
+
+
   return(
     <div className={style.wrapper}>
         <div className={style.regWrapper}>
           <div className={style.signUp}>
               <p>Реєстрація</p>
-              <form action="">
+              <form action="" onSubmit={handleSubmit}>
                 
-                <input type='text' id='FirstName' name='FirstName' placeholder='Введіть ім’я' required/>
-                <input type="text" name="LastName" id="LastName" placeholder='Введіть прізвище' />
-                <input type='email' id='email' name='email' placeholder='Введіть електронну адресу' required/>
-                <input type="password" id='password' name='password' placeholder='Введіть пароль' required/>
-                <input type="password"  id='repeatPassword' name='repeatPassword' placeholder='Повторіть пароль'/>
-                <button>Реєстрація</button>
+                <input type='text' id='FirstName' name='FirstName' value={formData.FirstName} onChange={handleChange} placeholder='Введіть ім’я' required/>
+                <input type="text" name="LastName" id="LastName" value={formData.LastName} onChange={handleChange} placeholder='Введіть прізвище'  required/>
+                <input type='email' id='Email' name='Email'value={formData.Email} onChange={handleChange} placeholder='Введіть електронну адресу' required/>
+                <input type="password" id='Password' name='Password' value={formData.Password} onChange={handleChange}placeholder='Введіть пароль' required/>
+                <input type="password"  id='RepeatPassword' name='RepeatPassword' placeholder='Повторіть пароль'/>
+                <button type='submit'>Реєстрація</button>
 
               </form>
           </div>
