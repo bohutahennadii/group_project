b680ed8e0c8d7ab6a977c87d9271cc9303f021f1
VoronovskiyVolodymyr <volodimir.voronovskiy@gmail.com>
2024-05-11 01:01:03 +0300

    bug fix

--- client/src/components/screens/AddAnnouncement/PageAddAnnouncement.jsx
+++ client/src/components/screens/AddAnnouncement/PageAddAnnouncement.jsx
@@ -1,32 +1,9 @@
 import React from 'react'
 // import Footer from '../home/footer/footer'
-import axios from 'axios';
-import { useState } from 'react';
+
 
 function PageAddAnnouncement() {
-  const [file, setFile] = useState(null);
-
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const formData = new FormData();
-      formData.append('photo', file);
-
-      await axios.post('http://localhost:3001/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-      console.log('Файл успішно відправлений на сервер');
-    } catch (error) {
-      console.error('Помилка відправлення файлу на сервер:', error);
-    }
-  };
+  
 
   return (
     <div>
--- client/src/components/screens/registrLogin/registr/regisrt.jsx
+++ client/src/components/screens/registrLogin/registr/regisrt.jsx
@@ -1,42 +1,10 @@
 import React from 'react'
 import style from './registr.module.css'
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
-import axios from 'axios';
+
 
 
 function Registr(){
-  const [formData, setFormData] = useState({
-    FirstName: '',
-    LastName: '',
-    Email: '',
-    Password: '',
-    
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:5042/api/Registration/registration', formData, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*' }
-        });
-      console.log('Успішно відправлено:', response.data);
-    } catch (error) {
-      console.error('Помилка відправки:', error);
-    }
-  }; 
-  
-
 
   return(
     <div className={style.wrapper}>
@@ -45,10 +13,10 @@ function Registr(){
               <p>Реєстрація</p>
               <form action="" onSubmit={handleSubmit}>
                 
-                <input type='text' id='FirstName' name='FirstName' value={formData.FirstName} onChange={handleChange} placeholder='Введіть ім’я' required/>
-                <input type="text" name="LastName" id="LastName" value={formData.LastName} onChange={handleChange} placeholder='Введіть прізвище'  required/>
-                <input type='email' id='Email' name='Email'value={formData.Email} onChange={handleChange} placeholder='Введіть електронну адресу' required/>
-                <input type="password" id='Password' name='Password' value={formData.Password} onChange={handleChange}placeholder='Введіть пароль' required/>
+                <input type='text' id='FirstName' name='FirstName'  placeholder='Введіть ім’я' required/>
+                <input type="text" name="LastName" id="LastName"  placeholder='Введіть прізвище'  required/>
+                <input type='email' id='Email' name='Email'  placeholder='Введіть електронну адресу' required/>
+                <input type="password" id='Password' name='Password' placeholder='Введіть пароль' required/>
                 <input type="password"  id='RepeatPassword' name='RepeatPassword' placeholder='Повторіть пароль'/>
                 <button type='submit'>Реєстрація</button>
 
